3c3b92b2e251c9874439cba20ebf32c14cc8c7b6
a <a@adeMacBook-Air.local>
2024-03-20 21:41:33 -0700

    Kill block main and reborn it in diferent shape

--- components/javascript/app.js
+++ components/javascript/app.js
@@ -6,20 +6,24 @@ var btn = document.getElementById("myBtn");
 
 var span = document.getElementsByClassName("close")[0];
 
-
+var body = document.getElementsByTagName("body")[0];
 
 btn.onclick = function() {
     modal.style.display = "block";
+    body.style.overflow = "hidden";
 }
 
 
 span.onclick = function() {
     modal.style.display = "none";
+    body.style.overflow = "auto";
 }
 
 
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
+        body.style.overflow = "auto";
     }
 }
+
