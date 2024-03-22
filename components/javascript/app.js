896b4e281acf010bef81a2022b0ae4315760a7b0
a <a@adeMacBook-Air.local>
2024-03-21 20:32:21 -0700

    Some changes are impactfull

--- components/javascript/app.js
+++ components/javascript/app.js
@@ -8,6 +8,16 @@ var span = document.getElementsByClassName("close")[0];
 
 var body = document.getElementsByTagName("body")[0];
 
+const burger = document.querySelector('.burger');
+const lines = document.querySelectorAll('.burger__line');
+
+function toggleBurger() {
+  lines.forEach((line) => line.classList.toggle('active'));
+}
+
+burger.addEventListener('click', toggleBurger);
+
+
 btn.onclick = function() {
     modal.style.display = "block";
     body.style.overflow = "hidden";
